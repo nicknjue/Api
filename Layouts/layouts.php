963cf845ea3762d97296b2353789bc6cbd714537
nicknjue <njue.wambui@strathmore.edu>
2025-09-04 18:40:32 +0300

    Created Autoload file

--- Layouts/layouts.php
+++ Layouts/layouts.php
@@ -1,7 +1,7 @@
 <?php
 class layouts{
-    public function heading() {
-       print "<h1>Hello world Welcome to BBIT</h1>";
+    public function heading($conf) {
+       print "<h1>Hello world Welcome to {$conf['site_name']}.</h1>";
     }
     public function footer($conf) {
        print "<footer>&copy;" .date("Y"). " {$conf['site_name']}.  All rights reserved.
